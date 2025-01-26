4de54798e656a4868cb4da26c4aa348f2e66f447
mariuste <mariustetard@googlemail.com>
2025-01-26 15:34:35 +0100

    Added port, added print job progress

--- Software/RemainingTimeDisplay/RemainingTimeDisplay.ino
+++ Software/RemainingTimeDisplay/RemainingTimeDisplay.ino
@@ -38,13 +38,13 @@ IPAddress ip(ip_address);
   // random port forwarded to the OctoPrint server from your router.
   // Enter that port here if you are external
   */
-const int octoprint_httpPort = 80;
+const int octoprint_httpPort = CONFIG_PORT;
 String octoprint_apikey = SECRET_API; //See top of file or GIT Readme about getting API key
 
 // Initialize OctoPrint API
 OctoprintApi api(client, ip, octoprint_httpPort, octoprint_apikey);
 
-unsigned long api_mtbs = 60000; //mean time between api requests (60 seconds)
+unsigned long api_mtbs = 5000; //mean time between api requests (5 seconds)
 unsigned long api_lasttime = 0;   //last time api request has been done
 
 // Setup Neopixel (no of neopizel, Pinnumber, type)
@@ -175,7 +175,7 @@ void loop() {
       pixels.setPixelColor(0, pixels.Color(0, 20, 0)); // green
       pixels.show();
       delay(100);
-      return;
+      // return;
       break;
     default:
       Serial.print("[WiFi] WiFi Status: ");
@@ -224,6 +224,50 @@ void loop() {
         Serial.println("------------------------");
       }
     }
+    if (api.getPrintJob()) {
+    if ((api.printJob.printerState == "Printing")) {
+          //FastLED.clear();
+          // we are printing something....
+          Serial.print("Progress:\t");
+          Serial.print(api.printJob.progressCompletion);
+          Serial.println(" %");
+          
+          //int progress = int((NUM_LEDS * int(api.printJob.progressCompletion)) / 100);
+          
+          //Set at least one pixel on when the job starts, as the strip may look 'off' for some time.
+          /*if(progress<1){
+            progress = 1;
+          }*/
+          
+          //fill_solid(leds, progress, CRGB::Green);
+          delay(100);
+          //FastLED.show();
+
+          //printed_timeout_timer = 0;
+        }
+        else if (api.printJob.progressCompletion == 100 && api.printJob.printerState == "Operational")
+        {
+          // 100% complete is no longer "Printing" but "Operational"
+          //FastLED.clear();
+          Serial.print("Progress:\t");
+          Serial.print(api.printJob.progressCompletion);
+          Serial.println(" %");
+          
+          //int progress = int(NUM_LEDS); //FULL LEDs
+        
+          //fill_solid(leds, progress, CRGB::Green);
+          delay(100);
+          //FastLED.show();
+        }
+        else if (api.printJob.printerState == "Offline" || api.printJob.printerState == "Operational")
+        {
+          // we are without working printer.... or the printer is no longer printing.... lights off
+          //fill_solid(leds, NUM_LEDS, CRGB::Black);
+          delay(100);
+          //FastLED.show();
+        }
+      }
+
     api_lasttime = millis();  //Set api_lasttime to current milliseconds run
   }
 }
--- Software/RemainingTimeDisplay/secret.h
+++ Software/RemainingTimeDisplay/secret.h
@@ -1,6 +1,7 @@
 // Printer Configutation
-#define CONFIG_NAME "Drucker 1" // Printer display name
-#define CONFIG_IP  "192.168.1.99" // IP Adress of printer
+#define CONFIG_NAME "Drucker 1" 	// Printer display name
+#define CONFIG_IP  	"192.168.1.99" 	// IP Adress of printer
+#define CONFIG_PORT 80        		// Port of the printer
 
 // Octoprint API key
 #define SECRET_API "API_KEY"
