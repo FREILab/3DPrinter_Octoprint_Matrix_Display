de1bfc335e8562113d07a54bd83f833ca1b7c4cf
mariuste <mariustetard@googlemail.com>
2025-02-16 17:18:26 +0100

    Update secret.h

--- Software/RemainingTimeDisplay/secret.h
+++ Software/RemainingTimeDisplay/secret.h
@@ -1,10 +1,10 @@
 // Printer Configutation
-#define CONFIG_NAME "Drucker 1" 	// Printer display name
+#define CONFIG_NAME "My printer name" 	// Printer display name
 #define CONFIG_IP  	"192.168.1.99" 	// IP Adress of printer
 #define CONFIG_PORT 80        		// Port of the printer
 
 // Octoprint API key
-#define SECRET_API "API_KEY"
+#define SECRET_API "API_KEY"  // enter the API key here
 
 // WiFi credentials
 #define SECRET_SSID  "MY_SSID"          // your network SSID (name)
